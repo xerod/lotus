a4c1411d9a27682f2d1e2e44ed9622db1ae3a22d
Akramurridjal Rahman <xerod@outlook.com>
2020-06-10 01:26:46 +0700

    First Commit

---
+++ fiddle/components/introduction.md
@@ -0,0 +1,11 @@
+---
+title: "contoh"
+description: ""
+sidebar: "docs"
+---
+
+# Introduction
+
+{{ $frontmatter.sidebar }}
+
+This page is for component page
---
+++ gridsome.config.js
@@ -0,0 +1,76 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: "Docc",
+  icon: {
+    favicon: "./src/assets/favicon.png",
+    touchicon: "./src/assets/favicon.png",
+  },
+  siteUrl: process.env.SITE_URL ? process.env.SITE_URL : "https://example.com",
+  settings: {
+    web: process.env.URL_WEB || false,
+    twitter: process.env.URL_TWITTER || false,
+    github: process.env.URL_GITHUB || false,
+    nav: {
+      links: [{ path: "/docs/", title: "Docs" }],
+    },
+    sidebar: [
+      {
+        name: "docs",
+        sections: [
+          {
+            title: "Getting Started",
+            items: [
+              "/docs/",
+              "/docs/installation/",
+              "/docs/writing-content/",
+              "/docs/deploying/",
+            ],
+          },
+          {
+            title: "Configuration",
+            items: ["/docs/settings/", "/docs/sidebar/"],
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    {
+      use: "gridsome-plugin-tailwindcss",
+      options: {
+        tailwindConfig: "./tailwind.config.js",
+        purgeConfig: {
+          // Prevent purging of prism classes.
+          whitelistPatternsChildren: [/token$/],
+        },
+      },
+    },
+
+    {
+      use: "@gridsome/plugin-google-analytics",
+      options: {
+        id: process.env.GA_ID ? process.env.GA_ID : "XX-999999999-9",
+      },
+    },
+
+    {
+      use: "@gridsome/plugin-sitemap",
+      options: {},
+    },
+
+    {
+      use: "@gridsome/vue-remark",
+      options: {
+        typeName: "Documentation", // Required
+        baseDir: "./content/docs", // Where .md files are located
+        pathPrefix: "/docs", // Add route prefix. Optional
+        template: "./src/templates/MarkdownPage.vue", // Optional
+      },
+    },
+  ],
+};
